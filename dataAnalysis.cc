a9ad45403cd7b50a9d09fc139fdf5fcd13c03f95
Oli806 <OJLi1@MY.SCCCD.EDU>
2025-04-01 16:52:58 -0700

    parsed file

---
+++ dataAnalysis.cc
@@ -0,0 +1,41 @@
+#include "/public/read.h"
+#include <cstdlib>
+#include <sstream>
+#include <fstream>
+#include <vector>
+#include <algorithm>
+#include <set>
+#include <map>
+using namespace std;
+
+int main() {
+	ifstream ins("DoorTrafficData.csv");
+
+	if (!ins) {
+		cerr << "could not open" << endl;
+		return 1;
+	}
+	string line = readline(ins); //get rid of first line 
+	vector<vector<string>> csv_file;
+
+	while (ins) { //parsing file
+		string line = readline(ins);
+		if (!ins) break;
+		istringstream iss(line);
+		vector<string> row;
+		while (iss) {
+			string str = readline(iss, ',');
+			if (!iss) break;
+			row.push_back(str);
+		}
+		csv_file.push_back(row);
+	}
+
+	for (const auto &row : csv_file) { //output parse file
+		for (const auto &col : row) {
+			cout << col << '\t';
+		}
+		cout << endl;
+	}
+
+}
